82c0740beb96496329fc19495ad603d77e671fbb
Hannah Bush <heb.rutgers@gmail.com>
2022-05-01 19:13:51 -0400

    product routes added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -85,7 +85,7 @@ router.delete('/:id', (req, res) => {
   })
     .then(dbTagData => {
       if (!dbTagData) {
-        res.status(404).json({ message: 'No tag found with this id' });
+        res.status(404).json({ message: 'No post found with this id' });
         return;
       }
       res.json(dbPostData);
