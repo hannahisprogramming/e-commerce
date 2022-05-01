b45a297741f4d115b7d7f5c63f25c37a71d4c42f
Hannah Bush <heb.rutgers@gmail.com>
2022-05-01 14:52:48 -0400

    added create, update and delete routes for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,11 +14,11 @@ router.get('/', (req, res) => {
       }
     ]
   })
-  .then(dbCatData => res.json(dbCatData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then(dbCatData => res.json(dbCatData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
@@ -32,29 +32,59 @@ router.get('/:id', (req, res) => {
       }
     ]
   })
-  .then(dbCatData => {
-    if(!dbCatData){
-      res.status(404).json({ message: 'No category found with this id' });
-      return;
-    }
-    res.json(dbCatData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then(dbCatData => {
+      if (!dbCatData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbCatData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(dbCatData => res.json(dbCatData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, { Product })
+    .then(updatedCatData => res.json(updatedCatData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCatData => {
+      if (!dbCatData) {
+        res.status(404).json({ message: 'No category found with this id!' });
+        return;
+      }
+      res.json(dbCatData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
