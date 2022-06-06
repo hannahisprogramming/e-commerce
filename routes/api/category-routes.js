282dbce450ffeb7abe39783732382bd24059ab5f
Hannah Bush <heb.rutgers@gmail.com>
2022-06-05 23:28:57 -0400

    updated tag findOne

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,6 +25,9 @@ router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   Category.findOne({
+    where: {
+      id: req.params.id
+    },
     include: [
       {
         model: Product,
